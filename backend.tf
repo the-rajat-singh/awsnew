d710b80bb842674e5067c3203bbc89d8c20ce234
the-rajat-singh <rs9460365919@gmail.com>
2025-11-11 18:34:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,9 @@
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "storage_for_rajat"
+    storage_account_name = "storagetfstateviarajat"
+    container_name       = "terra"
+    key                  = "terraform.tfstate" 
+  }
+}
