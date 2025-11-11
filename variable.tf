63f8134b7dca7239f6ff3123068df0b1fd877c9b
the-rajat-singh <rs9460365919@gmail.com>
2025-11-11 18:29:13 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "location" {
+  type    = string
+  default = "centralindia"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "rg-test-terraform"
+}
+
+variable "vnet_name" {
+  type    = string
+  default = "test-vnet"
+}
